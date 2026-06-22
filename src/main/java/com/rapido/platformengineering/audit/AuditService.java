e6830b6db3b4a33d8451839cdd08e85ce61d35c7
Nagesh024 <167857550+Nagesh024@users.noreply.github.com>
2026-06-22 16:50:18 +0530

    Save audit and sonar changes

--- src/main/java/com/rapido/platformengineering/audit/AuditService.java
+++ src/main/java/com/rapido/platformengineering/audit/AuditService.java
@@ -1,5 +1,31 @@
 package com.rapido.platformengineering.audit;
 
+import com.rapido.platformengineering.entity.AuditEvent;
+import com.rapido.platformengineering.repository.AuditEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class AuditService {
 
+    private final AuditEventRepository repository = null;
+
+    public void save(
+            String type,
+            String actor,
+            String action
+    ) {
+
+        AuditEvent event = AuditEvent.builder()
+                .eventType(type)
+                .actor(actor)
+                .action(action)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        repository.save(event);
+    }
 }
